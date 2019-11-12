016b194d58dcd3560dbf6425a9203b4bd44baaac
Халион <xalion67@gmail.com>
2019-11-12 09:12:43 +0300

    fix merge conflict

--- enzymes.py
+++ enzymes.py
@@ -2,27 +2,6 @@ import db
 
 
 class Enzymes_List():
-<<<<<<< HEAD
-    def __init__(self, enzymes_id):
-        connection, cursor = db.connect_to('enzymes.db')
-        response = cursor.execute("SELECT * FROM enzymes WHERE id=?", (enzymes_id, ))
-        data = response.fetchall()
-        if not data:
-            self.is_search = False
-        else:
-            self.is_search = True
-            self.enzymes_dict = {}
-            for index, enzymes in enumerate(data):
-                self.enzymes_dict.update({index: (enzymes[0], enzymes[1], enzymes[2], enzymes[3])})
-            self.enzymes_list = [Enzymes(id, name, amount, volume)
-                                 for id, name, amount, volume in self.enzymes_dict.values()]
-
-    def prepare_to_send(self):
-        enzymes_list = []
-        for enzymes in self.enzymes_list:
-            enzymes_list.append(f'Реактив {enzymes.name}, ID: {enzymes.id}\nКоличество: {enzymes.amount} – {enzymes.volume}')
-        return enzymes_list
-=======
     def __init__(self, id):
         connection, cursor = db.connect_to("enzymes.db")
         response = cursor.execute("SELECT * FROM enzymes WHERE id=?", (id, ))
@@ -42,23 +21,9 @@ class Enzymes_List():
         return '\n'.join(list_for_join)
 
 
-
->>>>>>> 5522b503b2071c9e4314c0669e1dfa8e6ab7d758
-
-
 class Enzymes():
     def __init__(self, id, name, amount, volume):
         self.id = id
-<<<<<<< HEAD
-        self.name = name
-        self.amount = amount
-        self.volume = volume
-=======
         self.name = name         
         self.amount = amount
         self.volume = volume
-
-
-search_enzymes = Enzymes_List('R005')
-search_enzymes.get_list_enzymes()
->>>>>>> 5522b503b2071c9e4314c0669e1dfa8e6ab7d758
